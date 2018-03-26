670602147fdec4553b333415fcaabff323a1a427
svd27 <sdjuric68@gmail.com>
2018-03-26 23:57:29 +0200

    KI-41 create basic event classes for #KI-41 Closed made property update event contain potentially multiple updates

--- core/common/src/main/kotlin/info/kinterest/events.kt
+++ core/common/src/main/kotlin/info/kinterest/events.kt
@@ -1,10 +1,14 @@
 package info.kinterest
 
+import info.kinterest.meta.KIEntityMeta
+import info.kinterest.meta.KIProperty
+
 sealed class KIEvent
-sealed class EntityEvent<E:KIEntity<T>,T:Comparable<T>>(val entity: E) : KIEvent()
-class EntityCreateEvent<E:KIEntity<K>, K:Comparable<K>>(e:E) : EntityEvent<E,K>(e)
-class EntityDeleteEvent<E:KIEntity<K>, K:Comparable<K>>(entity : E,val k:K) : EntityEvent<E,K>(entity)
-class EntityUpdateEvent<E:KIEntity<K>, K:Comparable<K>,V:Any>(e:E, val prop:String, val old:V?) : EntityEvent<E,K>(e)
+sealed class EntityEvent<E:KIEntity<K>,K:Comparable<K>>(val id: K, val meta: KIEntityMeta<K>) : KIEvent()
+class EntityCreateEvent<E:KIEntity<K>, K:Comparable<K>>(id:K, meta: KIEntityMeta<K>) : EntityEvent<E,K>(id, meta)
+class EntityDeleteEvent<E:KIEntity<K>, K:Comparable<K>>(id:K, meta: KIEntityMeta<K>) : EntityEvent<E,K>(id, meta)
+class EntityUpdatedEvent<E:KIEntity<K>, K:Comparable<K>>(id:K, meta: KIEntityMeta<K>, val updates:Iterable<EntityUpdateEvent<*>>) : EntityEvent<E,K>(id, meta)
+class EntityUpdateEvent<V:Any>(val prop:KIProperty<V>, val old:V?, val new:V?)
 sealed class KIErrorEvent : KIEvent()
-sealed class KIRecoverableErrorEvent(val msg:String)
-sealed class KIFatalError(val ex:Throwable)
+sealed class KIRecoverableErrorEvent(val msg:String, val ex:Exception?=null)
+sealed class KIFatalErrorEvent(val ex:Throwable)
