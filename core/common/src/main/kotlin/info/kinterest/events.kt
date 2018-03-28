70239663532a98f48401b3e7e4270f3a7e7b2a8f
svd27 <sdjuric68@gmail.com>
2018-03-28 12:31:55 +0200

    changed generics

--- core/common/src/main/kotlin/info/kinterest/events.kt
+++ core/common/src/main/kotlin/info/kinterest/events.kt
@@ -4,11 +4,11 @@ import info.kinterest.meta.KIEntityMeta
 import info.kinterest.meta.KIProperty
 
 sealed class KIEvent
-sealed class EntityEvent<E:KIEntity<K>,K:Comparable<K>>(val id: K, val meta: KIEntityMeta<K>) : KIEvent()
+sealed class EntityEvent<out E:KIEntity<K>,out K:Comparable<K>>(val id: K, val meta: KIEntityMeta<K>) : KIEvent()
 class EntityCreateEvent<E:KIEntity<K>, K:Comparable<K>>(id:K, meta: KIEntityMeta<K>) : EntityEvent<E,K>(id, meta)
 class EntityDeleteEvent<E:KIEntity<K>, K:Comparable<K>>(id:K, meta: KIEntityMeta<K>) : EntityEvent<E,K>(id, meta)
 class EntityUpdatedEvent<E:KIEntity<K>, K:Comparable<K>>(id:K, meta: KIEntityMeta<K>, val updates:Iterable<EntityUpdateEvent<*>>) : EntityEvent<E,K>(id, meta)
-class EntityUpdateEvent<V:Any>(val prop:KIProperty<V>, val old:V?, val new:V?)
-sealed class KIErrorEvent<T:KIError> : KIEvent()
+class EntityUpdateEvent<out V:Any>(val prop:KIProperty<V>, val old:V?, val new:V?)
+sealed class KIErrorEvent<out T:KIError> : KIEvent()
 class KIRecoverableErrorEvent(val msg:String, val ex:Exception?=null) : KIErrorEvent<KIRecoverableError>()
 class KIFatalErrorEvent(val msg:String, val ex:Throwable) : KIErrorEvent<KIFatalError>()
