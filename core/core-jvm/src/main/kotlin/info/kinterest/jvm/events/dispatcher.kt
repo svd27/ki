4b533875b0b141ad5fd6259bc70271f55bb7a7d4
svd27 <sdjuric68@gmail.com>
2018-03-28 12:16:34 +0200

    change variance

--- core/core-jvm/src/main/kotlin/info/kinterest/jvm/events/dispatcher.kt
+++ core/core-jvm/src/main/kotlin/info/kinterest/jvm/events/dispatcher.kt
@@ -1,13 +1,13 @@
 package info.kinterest.jvm.events
 
 import kotlinx.coroutines.experimental.CommonPool
-import kotlinx.coroutines.experimental.channels.Channel
+import kotlinx.coroutines.experimental.channels.*
 import kotlinx.coroutines.experimental.launch
 
-class Dispatcher<T> {
-    val incoming : Channel<T> = Channel()
-    private var outgoing : List<Channel<T>> = listOf()
-    val subscribing : Channel<Channel<T>> = Channel()
+class Dispatcher<out T> {
+    val incoming : ReceiveChannel<T> = Channel()
+    private var outgoing : List<SendChannel<T>> = listOf()
+    val subscribing : ReceiveChannel<SendChannel<T>> = Channel()
      init {
          launch(CommonPool) {
              for(s in subscribing) {
