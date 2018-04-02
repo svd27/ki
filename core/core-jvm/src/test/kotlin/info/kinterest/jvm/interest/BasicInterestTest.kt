37ba6feebe69147abed46f5be310d0052aeccfca
svd27 <sdjuric68@gmail.com>
2018-04-02 20:58:16 +0200

    KI-61 unit test nothing done

---
+++ core/core-jvm/src/test/kotlin/info/kinterest/jvm/interest/BasicInterestTest.kt
@@ -0,0 +1,15 @@
+package info.kinterest.jvm.interest
+
+import com.nhaarman.mockito_kotlin.whenever
+import info.kinterest.DataStore
+
+import org.amshove.kluent.mock
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.given
+
+class BasicInterestTest : Spek({
+    given("a datastore") {
+        val ds: DataStore = mock()
+        whenever(ds.q)
+    }
+})
